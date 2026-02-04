ece47bb542636c7206c957573f28fa5a14efef4e
anton62k <anton.kashirov@gmail.com>
2026-02-04 18:48:09 +0300

    feat: added tests

--- src/model/schema-model/__tests__/SchemaModel.addMovePatch.spec.ts
+++ src/model/schema-model/__tests__/SchemaModel.addMovePatch.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from '@jest/globals';
 import { createSchemaModel } from '../SchemaModelImpl.js';
 import { obj, num } from '../../../mocks/schema.mocks.js';
-import { findNodeIdByName } from './test-helpers.js';
+import { findNodeIdByName, findNestedNodeId } from './test-helpers.js';
 
 describe('SchemaModel add + move patch generation', () => {
   describe('move field into newly created parent', () => {
@@ -41,6 +41,51 @@ describe('SchemaModel add + move patch generation', () => {
 
       model.moveNode(aId!, groupId!);
       model.moveNode(bId!, groupId!);
+
+      expect(model.patches).toMatchSnapshot();
+    });
+  });
+
+  describe('add + move + remove moved field', () => {
+    it('generates only add patch when moved field is then removed', () => {
+      const model = createSchemaModel(
+        obj({
+          value: num(),
+          sum: num(),
+        }),
+      );
+
+      const rootId = model.root.id();
+      model.addField(rootId, 'nested', 'object');
+
+      const sumId = findNodeIdByName(model, 'sum');
+      const nestedId = findNodeIdByName(model, 'nested');
+      model.moveNode(sumId!, nestedId!);
+
+      const movedSumId = findNestedNodeId(model, 'nested', 'sum');
+      model.removeField(movedSumId!);
+
+      expect(model.patches).toMatchSnapshot();
+    });
+  });
+
+  describe('add + move + remove new parent', () => {
+    it('generates only remove patch when new parent with moved children is removed', () => {
+      const model = createSchemaModel(
+        obj({
+          value: num(),
+          sum: num(),
+        }),
+      );
+
+      const rootId = model.root.id();
+      model.addField(rootId, 'nested', 'object');
+
+      const sumId = findNodeIdByName(model, 'sum');
+      const nestedId = findNodeIdByName(model, 'nested');
+      model.moveNode(sumId!, nestedId!);
+
+      model.removeField(nestedId!);
 
       expect(model.patches).toMatchSnapshot();
     });
