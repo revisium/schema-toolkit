deaaa210a26ac855f2183811e00ddbd9e708af43
anton62k <anton.kashirov@gmail.com>
2026-02-08 10:50:09 +0300

    fix index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,4 @@ export * from './json-patch.types.js';
 export * from './json-value-patch.types.js';
 export * from './migration.types.js';
 export * from './schema.types.js';
-export * from './typed-node.types.js';
 export * from './value-diff.types.js';
